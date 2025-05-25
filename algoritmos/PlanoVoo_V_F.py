3b6c18e8c692f1ff71cba814f06ea557cd7d054f
Leandro França <geoleandro.franca@gmail.com>
2025-05-25 12:38:25 -0300

    Update PlanoVoo_V_F.py

--- algoritmos/PlanoVoo_V_F.py
+++ algoritmos/PlanoVoo_V_F.py
@@ -386,7 +386,7 @@ class PlanoVoo_V_F(QgsProcessingAlgorithm):
         return PlanoVoo_V_F()
 
     def tags(self):
-        return self.tr('Flight Plan,Measure,Topography,Fachada,Vertical,Front,View').split(',')
+        return self.tr('Flight Plan,Measure,Topography,Fachada,Vertical,Front,View,GeoOne').split(',')
 
     def icon(self):
         return QIcon(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images/Vertical.png'))
@@ -402,11 +402,17 @@ It enables the planning of a precise vertical trajectory with appropriate overla
 <p><span>The outputs are <b>CSV</b> file compatible with the <b>Litchi app</b>. and 2 Layers - <b>Flight Line</b> and <b>Photos Points</b>.
 <p>It can also be used with other flight applications, utilizing the 2 genereted Layers for flight lines and waypoints.</p>
 <p><b>
-<p><b>Tips:</b></p>
+<p><b>Learn more:</b><o:p></o:p></p>
 <ul style="margin-top: 0cm;" type="disc">
-  <li><span><a href="https://geoone.com.br/opentopography-qgis/">Obtain the MDE for the Open Topography plugin</a></span></li>
-  <li><a href="https://geoone.com.br/plano-de-voo-para-drone-com-python/#sensor">Check your drone sensor parameters</a></li>
-</ul>"""
+   <li><a href="https://geoone.com.br/pvplanodevoo">Sign up for GeoFlight Planner course</a><o:p></o:p></li>
+   <li><a href="https://portal.geoone.com.br/m/lessons/planodevoo?classId=5681">Click here to access the class with all the details about this tool!</a><o:p></o:p></li>
+</ul>
+<p><b>Tips:</b><o:p></o:p></p>
+<ul style="margin-top: 0cm;" type="disc">
+  <li><a href="https://geoone.com.br/plano-de-voo-para-drone-com-python/#sensor">Check your drone sensor parameters</a><o:p></o:p></li>
+  <li><a href="https://geoone.com.br/opentopography-qgis/">Obtain the MDE for the Open Topography plugin</a><o:p></o:p></span></li>
+</ul>
+"""
 
     figura = 'images/Facade.jpg'
 
